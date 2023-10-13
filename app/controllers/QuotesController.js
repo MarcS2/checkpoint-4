38c02399560889452e6683e4872ca03dc5ed0f1d
MarcS2 <marcellosharrai@hotmail.com>
2023-10-13 16:05:21 -0600

    checkpoint-4

--- app/controllers/QuotesController.js
+++ app/controllers/QuotesController.js
@@ -1,9 +1,19 @@
+import { AppState } from "../AppState.js";
 import { quotesService } from "../services/QuotesService.js";
 import { Pop } from "../utils/Pop.js";
+import { setHTML } from "../utils/Writer.js";
+
+
+function _drawActiveQuote() {
+  setHTML('quote', `<p class="fs-2 text-white text-center quote">${AppState.activeQuote.content}</p>
+  <p class="fs-4 text-white text-center author">${AppState.activeQuote.author}</p>`)
+}
+
 
 export class QuotesController {
   constructor() {
     this.getQuotes()
+    AppState.on('activeQuote', _drawActiveQuote)
   }
 
   async getQuotes() {
